6ce6446fe4f25e942ba8be374c90782961d9127c
JamesB-lab <67161761+JamesB-lab@users.noreply.github.com>
2021-09-28 20:21:33 +0100

    initial commit

---
+++ tello_course/image_capture.py
@@ -0,0 +1,15 @@
+# image capture code
+from djitellopy import tello
+import cv2
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+
+me.streamon()  # gives all the stream frame by frame for processing
+
+while True:
+    img = me.get_frame_read().frame
+    img = cv2.resize(img, (360, 240))
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)  # value in ms
